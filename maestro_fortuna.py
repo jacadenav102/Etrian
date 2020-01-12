9c3729762a5ef52603ab6c458066c136e570fa95
Antonio Cadena <antonocadena102@gmail.com>
2020-01-12 00:18:58 -0500

    Se agrega  la posibilidad de agregar un bonificado a las tiradas

--- maestro_fortuna.py
+++ maestro_fortuna.py
@@ -1,15 +1,17 @@
 import os
 import discord
 import random 
+import re
+import numpy as np
 from dotenv import load_dotenv
 from discord.ext import commands
 from dotenv import load_dotenv
-
+from numpy.random import choice
 
 
 load_dotenv()
 token = os.getenv('MAESTRO_FORTUNA')
-bot = commands.Bot(command_prefix='maestro')
+bot = commands.Bot(command_prefix='m')
 
 
 @bot.event
@@ -17,14 +19,20 @@ async def on_ready():
     print(f'maestro se ha conectado a Discord!')
     
 @bot.command(name='dados', help='Lanzamiento de dados')
-async def roll(ctx, numero_dados:int = 1, number_of_sides:int = 20):
+async def roll(ctx, numero_dados:int = 1, numero_caras:int = 20,bonificador: int = 0):
     """ 
     Documentación
     """
-    dado = [
-        str(random.choice(range(1, number_of_sides + 1)))
-        for _ in range(numero_dados)
-    ]
-    await ctx.send(', '.join(dado))
+    resultado = bonificador
+    dado = [str(choice(np.arange(1,stop = numero_caras+1,step=1))) for _ in range(numero_dados)] 
+    dados = ', '.join(dado)
+    
+    for valores in re.split(',',dados):
+        resultado = resultado + int(valores)
+
+    final = str(resultado) + ' <-- '  + dados  + ' + ' + str(bonificador)
+    
+    
+    await ctx.send(final)
     
 bot.run(token)
