011fbc7844a43042fb7f9baa6d55943a48ec13b8
Antonio Cadena <antonocadena102@gmail.com>
2020-01-11 22:53:04 -0500

    Base de datos de las respuestas del chatbot

---
+++ respuestas.py
@@ -0,0 +1,21 @@
+lista_respuestas ={
+    'Hola' :['Hola ¿Como estas?'],
+    'partida' : ['Te enviaré una mensaje con mas información.'],
+    'partida' : ['Te enviaré una mensaje con mas información.'],
+    'Precio' : ['Puedes pagar con efectivo o haciendo una donación.'],
+    'precio' : ['Puedes pagar con efectivo o haciendo una donación.'],
+    '¿Como te llamas?':['Me llaman Asibot', 'El nombre es Issac, Isacc Asibot','Asimov, Asibot para los amigos.'],
+    'Rol' : ['Tenemos diferentes juegos.Cuentame mas sobre lo que estas buscando.'],
+    'rol' : ['Tenemos diferentes juegos.Cuentame mas sobre lo que estas buscando.'],
+    'Calabozo y Dragones': ['Tenemos varias mesas de Calabozos y Dragones quinta edición, debes consultar la disponibilidad.'],
+    'Costo': ['Puedes pagar con efectivo o haciendo una donación.'],
+    'costo': ['Puedes pagar con efectivo o haciendo una donación.'],
+    'ubicados':['No tenemos sede fija, tenemos partidas en línea y aveces usamos espacio del centro de Medellin'],
+    'confusion': ['Parece que tengo un bug ¿Puedes repetir?','Lo siento no hablo elfico ¿Puedes hablarme en común?',
+                   'Lo siento, no soy como C-3PO, no conozco tantos idiomas ¿Puede repetir?']}
+
+lista_confusion = ['Parece que tengo un bug ¿Puedes repetir?','Lo siento no hablo elfico ¿Puedes hablarme en común?',
+                   'Lo siento, no soy como C-3PO, no conozco tantos idiomas ¿Puede repetir?']
+
+
+lista_negra = ['mierda','Hijo de puta']
