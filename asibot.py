1b3ffd562f5d2e0e813f9d204108b34057904e04
Antonio Cadena <antonocadena102@gmail.com>
2020-01-13 21:04:50 -0500

    Se corrige la lista negra

--- asibot.py
+++ asibot.py
@@ -8,8 +8,6 @@ from respuestas import lista_respuestas
 from respuestas import lista_negra
 from discord.ext import commands
 
-if 'partida' in lista_respuestas :
-    lista_respuestas['partida']
 
 
 load_dotenv()
@@ -28,13 +26,12 @@ async def on_member_remove(member):
 
 @bot.event
 async def on_message(message):
-
-    if (str(message.channel) == 'consulta-con-issac-asibot' or str(message.channel) == f'Direct Message with {message.author}') and str(message.author) !=  'Asibot#6995':
-                if crea_respusta_patrones(lista_respuestas,message.content) is None:
-                     await message.channel.send(rd.choice(lista_respuestas['confusion']))
-                    
+    print(message.author)
+    if (str(message.channel) == 'consulta-con-issac-asibot' or str(message.channel) == f'Direct Message with {message.author}') and str(message.author) !=  'Asibot#1675':
+                if crea_respusta_patrones(lista_respuestas,message.content) != None:
+                    await message.channel.send(crea_respusta_patrones(lista_respuestas,message.content))
                 else:
-                    await message.channel.send(rd.choice(crea_respusta_patrones(lista_respuestas,message.content)))
+                    await message.channel.send(lista_respuestas['confusion'])
 
     else:
         
